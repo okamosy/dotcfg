27843fa69171223ec66219215faa007a4f1c002a
Seiji Okamoto <seiji.okamoto@thirdbridge.com>
2018-01-16 16:53:42 +0000

    Add function for printout out the path nicely

--- .zsh/functions.zsh
+++ .zsh/functions.zsh
@@ -1,3 +1,4 @@
+# Compile zsh text files for better performance
 function zsh_recompile {
     autoload -U zrecompile
     rm -f ~/.zsh/*.zwc
@@ -13,4 +14,15 @@ function zsh_recompile {
     [[ -f ~/.zcompump.zwc.old ]] && rm -f ~/.zcompdump.zwc.old
 
     source ~/.zshrc
+}
+
+# Print out a nicely formatted and coloured path
+path() {
+    echo $PATH | tr ":" "\n" | \
+        awk "{ sub(\"/usr\", \"$fg_no_bold[green]/usr$reset_color\"); \
+            sub(\"/bin\", \"$fg_no_bold[blue]/bin$reset_color\"); \
+            sub(\"/opt\", \"$fg_no_bold[cyan]/opt$reset_color\"); \
+            sub(\"/sbin\", \"$fg_no_bold[magenta]/sbin$reset_color\"); \
+            sub(\"/local\", \"$fg_no_bold[yellow]/local$reset_color\"); \
+            print }"
 }
